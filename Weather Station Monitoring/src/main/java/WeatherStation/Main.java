9874154d67e09e985b88e0c67235dd3f72cdd6b2
abdelaziz <90736727+Abdelaziz25@users.noreply.github.com>
2024-05-04 22:15:19 +0300

    Finish modifing message status and drop messages

---
+++ Weather Station Monitoring/src/main/java/WeatherStation/Main.java
@@ -0,0 +1,33 @@
+package WeatherStation;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        // Set up Kafka producer properties
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        // Create Kafka producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        // Instantiate OpenMeteoAPI with the Kafka producer
+        OpenMeteoAPI weatherAdapter = new OpenMeteoAPI(producer);
+
+
+        // Geo coordinates
+        double latitude = 31.2018;
+        double longitude = 29.9158;
+
+        // Fetch and publish weather data
+        weatherAdapter.fetchAndPublishWeatherData(latitude, longitude);
+
+        // Close the Kafka producer
+        producer.close();
+    }
+}
---
+++ Weather Station Monitoring/src/main/java/WeatherStation/WeatherMessageBuilder.java
@@ -0,0 +1,37 @@
+package WeatherStation;
+
+import org.json.JSONObject;
+
+import java.util.Random;
+
+public class WeatherMessageBuilder {
+    // Random generator for battery status
+
+    // Method to build weather message with specified attributes
+    public static JSONObject buildWeatherMessage(int stationId, int sNo, int relativeHumidity, int windSpeed, int temperature, long time) {
+        JSONObject kafkaMessage = new JSONObject();
+        kafkaMessage.put("station_id", stationId);
+        kafkaMessage.put("s_no", sNo);
+        kafkaMessage.put("battery_status", getRandomBatteryStatus());
+        kafkaMessage.put("status_timestamp", time);
+        JSONObject weatherData = new JSONObject();
+        weatherData.put("humidity", relativeHumidity);
+        weatherData.put("temperature", temperature);
+        weatherData.put("wind_speed", windSpeed);
+        kafkaMessage.put("weather", weatherData);
+        return kafkaMessage;
+    }
+
+    // Method to randomly select battery status
+    private static String getRandomBatteryStatus() {
+        Random random = new Random();
+        double probability = random.nextDouble();
+        if (probability < 0.4) {
+            return "Medium"; // Medium battery
+        } else if (probability < 0.7) {
+            return "Low"; // Low battery
+        } else {
+            return "High"; // High battery
+        }
+    }
+}
