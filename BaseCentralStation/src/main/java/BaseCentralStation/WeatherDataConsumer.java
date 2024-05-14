77f47fe54e7a59a784804c640aedee1c8bba599c
abdelaziz <90736727+Abdelaziz25@users.noreply.github.com>
2024-05-14 10:15:04 +0300

    finish writing in container and partition directories

--- BaseCentralStation/src/main/java/BaseCentralStation/WeatherDataConsumer.java
+++ BaseCentralStation/src/main/java/BaseCentralStation/WeatherDataConsumer.java
@@ -1,5 +1,4 @@
 package BaseCentralStation;
-
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
@@ -19,76 +18,64 @@ import org.apache.parquet.hadoop.util.HadoopOutputFile;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.json.JSONObject;
-
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Properties;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class WeatherDataConsumer {
-    private static final int BATCH_SIZE = 50;
-
+    private static final int BATCH_SIZE = 100;
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
+    public static String projectDir = System.getProperty("user.dir");
+    public static int fileIndex =0;
     public static void main(String[] args) throws IOException {
-        String projectDir = System.getProperty("user.dir");
         String avroSchemaFilePath = projectDir + "/src/main/java/BaseCentralStation/weather_record.avsc";
-
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-release.kafka:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "weather-data-consumer-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Collections.singletonList("Project"));
-
         Schema avroSchema = new Schema.Parser().parse(new File(avroSchemaFilePath));
-
-        List<GenericRecord> recordsBatch = new ArrayList<>();
         int recordCount = 0;
-        int fileIndex = 0;
+        Map<Integer, List<GenericRecord>> stationRecordsMap = new HashMap<>();
+            System.out.println("lola");
+            for(int i=1;i<11;i++)
+            {
+                stationRecordsMap.put(i, new ArrayList<>());
+//                File stationDir = new File("/mnt/Volume"+"/station"+i);
+//                if (!stationDir.exists()) stationDir.mkdir();
+            }
 
-        ParquetWriter<GenericRecord> parquetWriter = null;
-//       for(int i=1;i<11;i++)
-//       {
-//           File stationDir = new File("/mnt/c/Users/abdel/Desktop/Connect-4/Weather-Stations-Monitoring/BaseCentralStation/Volume/station"+i);
-//           if (!stationDir.exists()) stationDir.mkdir();
-//       }
+
         try {
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
 
                 for (ConsumerRecord<String, String> record : records) {
                     GenericRecord avroRecord = createAvroRecord(record.value(), avroSchema);
-                    recordsBatch.add(avroRecord);
                     recordCount++;
                     System.out.println(record.value());
+                    int stationId = (int) avroRecord.get("station_id");
+                    List<GenericRecord> stationRecords = stationRecordsMap.get(stationId);
+                    stationRecords.add(avroRecord);
+                    System.out.println(stationId);
                     System.out.println(recordCount);
-                    System.out.println("here_here");
+                    System.out.println("heello");
                     if (recordCount >= BATCH_SIZE) {
                         // Write the batch to a Parquet file
                         System.out.println("l2");
-                        String parquetFilePath = "/mnt/c/Users/abdel/Desktop/Connect-4/Weather-Stations-Monitoring/BaseCentralStation/Volume/Project"+ fileIndex + ".parquet";
-                        Path path = new Path(parquetFilePath);
-                        fileIndex++;
-                        System.out.println(fileIndex);
-                        System.out.println("l3");
-                        System.out.println("l4");
-                        parquetWriter = AvroParquetWriter.<GenericRecord>builder(path).withSchema(avroSchema).withCompressionCodec(CompressionCodecName.SNAPPY)
-                                    .build();
                         try {
                             System.out.println("l5");
-                            for (GenericRecord batchRecord : recordsBatch) {
-                                parquetWriter.write(batchRecord);
-                            }
+                            writeRecordsToParquet(stationId, stationRecords, avroSchema);
                         } catch (IOException e) {
-                            System.out.println("llll");
                             e.printStackTrace();
                             // Optionally handle the exception, e.g., skip the batch
                         } finally {
-                            recordsBatch.clear();
+                            stationRecords.clear();
                             recordCount = 0;
                         }
                     }
@@ -96,29 +83,46 @@ public class WeatherDataConsumer {
             }
         } finally {
             consumer.close();
-            if (parquetWriter != null) {
-                parquetWriter.close(); // Close Parquet writer outside the loop
-            }
         }
     }
 
     private static GenericRecord createAvroRecord(String jsonString, Schema schema) throws IOException {
         JSONObject json = new JSONObject(jsonString);
         GenericRecordBuilder recordBuilder = new GenericRecordBuilder(schema);
-
         recordBuilder.set("station_id", json.getInt("station_id"));
         recordBuilder.set("s_no", json.getInt("s_no"));
         recordBuilder.set("status_timestamp", json.getLong("status_timestamp"));
         recordBuilder.set("battery_status", json.getString("battery_status"));
-
         JSONObject weatherJson = json.getJSONObject("weather");
         GenericRecord weatherRecord = new GenericData.Record(schema.getField("weather").schema());
         weatherRecord.put("temperature", weatherJson.getInt("temperature"));
         weatherRecord.put("humidity", weatherJson.getInt("humidity"));
         weatherRecord.put("wind_speed", weatherJson.getInt("wind_speed"));
-
         recordBuilder.set("weather", weatherRecord);
-
         return recordBuilder.build();
+    }
+    private static void writeRecordsToParquet(int stationId, List<GenericRecord> records, Schema schema) throws IOException {
+        System.out.println(stationId);
+        LocalDate today = LocalDate.now();
+        String dateDirName = today.format(DATE_FORMATTER);
+        System.out.println(dateDirName);
+        String stationDirPath = "/mnt/Volume" + "/station" + stationId +"/"+dateDirName ;
+////        File dateDir = new File(stationDirPath);
+////        if (!dateDir.exists()) {
+////            dateDir.mkdirs();
+////        }
+        System.out.println(System.currentTimeMillis());
+        System.out.println(fileIndex);
+        String parquetFilePath = stationDirPath+ "/Project_"+ System.currentTimeMillis()+ ".parquet";;
+        Path path = new Path(parquetFilePath);
+        ParquetWriter<GenericRecord> parquetWriter = null;
+        parquetWriter = AvroParquetWriter.<GenericRecord>builder(path).withSchema(schema).build();
+            for (GenericRecord record : records) {
+                parquetWriter.write(record);
+            }
+            if (parquetWriter != null) {
+                parquetWriter.close(); // Close Parquet writer outside the loop
+            }
+            fileIndex++;
     }
 }
